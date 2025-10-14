0b34de508cd9a9bc1489f0aba55c4c1c9e4be889
Paul <p@1g4.org>
2025-10-14 06:45:22 -0500

    restore tab titles

--- featherpad/fpwin.cpp
+++ featherpad/fpwin.cpp
@@ -5517,6 +5517,7 @@ void FPwin::aboutDialog() {
     dialog.setMainText("<center> " + tr("A lightweight, tabbed, plain-text editor") + " </center>\n<center> " +
                        tr("based on Qt") + " </center><br><center> " + tr("Author") +
                        ": <a href='mailto:share@1g4.org?Subject=My%20Subject'>jopamo</a> </center><p></p>");
+    dialog.setTabTexts(tr("About FeatherPad"), tr("Translators"));
     dialog.setWindowTitle(tr("About FeatherPad"));
     dialog.setWindowModality(Qt::WindowModal);
     dialog.exec();
