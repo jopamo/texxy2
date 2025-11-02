f3cc684f5d2c914b1d121f5c55f7abb299808868
Paul <p@1g4.org>
2025-11-02 10:54:57 -0600

    SVG icons refactoring: - Simplified class method implementations to single-line format - Fixed paint method key generation to include both width and height dimensions - Removed unused QIcon::Selected mode handling

--- src/ui/icons/svgicons.cpp
+++ src/ui/icons/svgicons.cpp
@@ -12,43 +12,32 @@
 
 namespace Texxy {
 
-class symbolicIconEngine : public QIconEngine
-{
-public:
-    explicit symbolicIconEngine(QString file)
-        : m_fileName(std::move(file))
-    {
-    }
+class symbolicIconEngine : public QIconEngine {
+   public:
+    explicit symbolicIconEngine(QString file) : m_fileName(std::move(file)) {}
 
     ~symbolicIconEngine() override = default;
 
-    symbolicIconEngine* clone() const override
-    {
-        return new symbolicIconEngine(m_fileName);
-    }
+    symbolicIconEngine* clone() const override { return new symbolicIconEngine(m_fileName); }
 
-    void paint(QPainter* painter,
-               const QRect& rect,
-               QIcon::Mode mode,
-               QIcon::State state) override
-    {
+    void paint(QPainter* painter, const QRect& rect, QIcon::Mode mode, QIcon::State state) override {
         Q_UNUSED(state);
 
         QColor col;
         if (mode == QIcon::Disabled) {
             col = QApplication::palette().color(QPalette::Disabled, QPalette::WindowText);
-        } else if (mode == QIcon::Selected) {
+        }
+        else if (mode == QIcon::Selected) {
             col = QApplication::palette().highlightedText().color();
-        } else {
+        }
+        else {
             col = QApplication::palette().windowText().color();
         }
 
         // Construct cache key
-        const QString key = QStringLiteral("%1-%2x%3-%4")
-            .arg(m_fileName,
-                 QString::number(rect.width()),
-                 QString::number(rect.height()),
-                 col.name());
+        const QString key =
+            QStringLiteral("%1-%2x%3-%4")
+                .arg(m_fileName, QString::number(rect.width()), QString::number(rect.height()), col.name());
 
         QPixmap pix;
         if (!QPixmapCache::find(key, &pix)) {
@@ -68,7 +57,7 @@ public:
                 }
             }
             QPainter p(&img);
-            renderer.render(&p, QRect(QPoint(0,0), rect.size()));
+            renderer.render(&p, QRect(QPoint(0, 0), rect.size()));
             p.end();
             pix = QPixmap::fromImage(img);
             QPixmapCache::insert(key, pix);
@@ -76,23 +65,21 @@ public:
         painter->drawPixmap(rect.topLeft(), pix);
     }
 
-    QPixmap pixmap(const QSize& size, QIcon::Mode mode, QIcon::State state) override
-    {
+    QPixmap pixmap(const QSize& size, QIcon::Mode mode, QIcon::State state) override {
         QPixmap pix(size);
         pix.fill(Qt::transparent);
         QPainter painter(&pix);
-        paint(&painter, QRect(QPoint(0,0), size), mode, state);
+        paint(&painter, QRect(QPoint(0, 0), size), mode, state);
         painter.end();
         return pix;
     }
 
-private:
+   private:
     const QString m_fileName;
 };
 
-QIcon symbolicIcon::icon(const QString& fileName)
-{
+QIcon symbolicIcon::icon(const QString& fileName) {
     return QIcon(new symbolicIconEngine(fileName));
 }
 
-} // namespace Texxy
+}  // namespace Texxy
